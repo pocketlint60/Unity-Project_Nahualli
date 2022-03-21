c529c99c7c15ebadc221db65f3cee3d1e0bb4649
pocketlint60 <invaderethan@yahoo.com>
2022-03-21 13:14:27 -0700

    v0.0.3
    Created scripts for the other three forms.
    
    Imported placeholder movement code from StS.

--- Assets/Scripts/Player/BasePlayer.cs
+++ Assets/Scripts/Player/BasePlayer.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class BasePlayer : MonoBehaviour
 {
-    CharacterController playerControls;
+    //CharacterController pControls;
 
     //Basic player properties
-    protected float speedMult;
+    protected float speedMult = 0f;
+    protected float rotSpeed = 2f;
     protected float jumpHeight;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        playerControls = gameObject.GetComponent<CharacterController>();
+        //pControls = gameObject.GetComponent<CharacterController>();
+        //if (pControls == null)
+        {
+            print("Character Controller not found on " + gameObject.name);
+        }
     }
 
     // Update is called once per frame
@@ -23,9 +28,23 @@ public class BasePlayer : MonoBehaviour
         ControlMotion();
     }
 
-    public void ControlMotion()
+    public virtual void ControlMotion()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float forwardInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, forwardInput);
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+        movementDirection.Normalize();
+
+        transform.Translate((movementDirection * (speedMult) * inputMagnitude * Time.deltaTime), Space.World);
+
+        //Rotates player to face the direction of movement
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotSpeed);
+        }
     }
 
     public virtual void PlayProperties()
@@ -34,7 +53,6 @@ public class BasePlayer : MonoBehaviour
         // POLYMORPHISM - PlayProperties contains the basic properties of the player character which change depending on form
         //BasePlayer contains the default state held by the parent Player object; this object cannot move
 
-        speedMult = 0f;
         jumpHeight = 0f;
         PrimeFunction();
     }
--- Assets/Scripts/Player/Player_Flyer.cs
+++ Assets/Scripts/Player/Player_Flyer.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Human : BasePlayer
+public class Player_Flyer : BasePlayer
 {
-    
-
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,9 +17,9 @@ public class Player_Human : BasePlayer
     }
 
     public override void ControlMotion()
-    {        
+    {
         base.ControlMotion();
-        speedMult = 3f;
+        speedMult = 5f;
     }
 
     public override void PlayProperties()
